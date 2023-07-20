5f47feea96485e16a59da32abfb06a1b0ede492d
Rizkan-Dev-Win <riezkanaprianda@gmail.com>
2023-07-20 22:24:55 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b6681eeecf791cb5cc39f93a444a3f2f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a5fc7fe9-a529-435f-a853-86040449d669'
+};
